de1524d4fd65fa76176a7397af52e7f577eaa437
unknown <joderlan.silva@gmail.com>
2021-11-17 22:39:58 -0300

    ajustano admin  adminitens

--- estoque/admin.py
+++ estoque/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from .models import Estoque , EstoqueItens
  
 # Register your models here.
+class EstoqueItensAdmin(admin.TabularInline):
+    model =  EstoqueItens
+    estra = 0
 
 @admin.register(Estoque)
 
 class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensAdmin , )
     list_display = (
         '__str__',
         'nf',
